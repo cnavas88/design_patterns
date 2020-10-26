13d506216b32cd9d23c5171deafb80d3741c3af8
Carlos Navas <dante.maniacs@gmail.com>
2020-10-26 17:45:51 +0100

    adding all tests

--- python/test_coffeeshop.py
+++ python/test_coffeeshop.py
@@ -8,3 +8,30 @@ class MyTest(unittest.TestCase):
         self.assertEqual(coffee.get_cost(), 1.0)
         self.assertEqual(coffee.get_tax(), 0.1)
         self.assertEqual(coffee.get_ingredients(), "coffee")
+
+    def test_coffee_with_sugar(self):
+        coffee = coffeeshop.Concrete_Coffee()
+        coffee_with_sugar = coffeeshop.Sugar(coffee)
+
+        self.assertEqual(coffee_with_sugar.get_cost(), 1.0)
+        self.assertEqual(coffee_with_sugar.get_tax(), 0.1)
+        self.assertEqual(coffee_with_sugar.get_ingredients(), "coffee, sugar")
+
+    def test_coffee_with_sugar_milk(self):
+        coffee = coffeeshop.Concrete_Coffee()
+        coffee_with_sugar = coffeeshop.Sugar(coffee)
+        coffee_with_sugar_milk = coffeeshop.Milk(coffee_with_sugar)
+
+        self.assertEqual(coffee_with_sugar_milk.get_cost(), 1.25)
+        self.assertEqual(coffee_with_sugar_milk.get_tax(), 0.125)
+        self.assertEqual(coffee_with_sugar_milk.get_ingredients(), "coffee, sugar, milk")
+
+    def test_coffee_with_all(self):
+        coffee = coffeeshop.Concrete_Coffee()
+        coffee_with_sugar = coffeeshop.Sugar(coffee)
+        coffee_with_sugar_milk = coffeeshop.Milk(coffee_with_sugar)
+        coffee_with_all = coffeeshop.Vanilla(coffee_with_sugar_milk)
+
+        self.assertEqual(coffee_with_all.get_cost(), 2.00)
+        self.assertEqual(coffee_with_all.get_tax(), 0.2)
+        self.assertEqual(coffee_with_all.get_ingredients(), "coffee, sugar, milk, vanilla")
